64ec57a03cf726e93e44aa5e9469a63ff0ce0b4f
German Lena <german.lena@gmail.com>
2016-09-19 09:34:24 -0300

    fix error becuase of contract and class with the same name

--- src/Auth0/Login/Repository/Auth0UserRepository.php
+++ src/Auth0/Login/Repository/Auth0UserRepository.php
@@ -4,9 +4,9 @@ namespace Auth0\Login\Repository;
 
 use Auth0\Login\Auth0User;
 use Auth0\Login\Auth0JWTUser;
-use Auth0\Login\Contract\Auth0UserRepository;
+use Auth0\Login\Contract\Auth0UserRepository as Auth0UserRepositoryContract;
 
-class Auth0UserRepository implements Auth0UserRepository {
+class Auth0UserRepository implements Auth0UserRepositoryContract {
 
     public function getUserByDecodedJWT($jwt) {
         return new Auth0JWTUser($jwt);
