8971d0e4a88e01768ed8fb30b7e60f5134933ea9
Jack Skinner <jack@developerjack.com>
2018-03-19 00:47:43 +1100

    Make code comments gender neutral
    Replaces gendered references to users with a neutral 'them'.

--- src/controllers/Auth0Controller.php
+++ src/controllers/Auth0Controller.php
@@ -37,9 +37,9 @@ class Auth0Controller extends Controller
         $auth0User = $this->userRepository->getUserByUserInfo($profile);
 
         if ($auth0User) {
-            // If we have a user, we are going to log him in, but if
+            // If we have a user, we are going to log them in, but if
             // there is an onLogin defined we need to allow the Laravel developer
-            // to implement the user as he wants an also let him store it.
+            // to implement the user as he wants an also let them store it.
             if ($service->hasOnLogin()) {
                 $user = $service->callOnLogin($auth0User);
             } else {
