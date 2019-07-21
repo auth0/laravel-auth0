2ef9a8b1d5761affe75d4a2a3d34a80e837b09c4
Nico Stapelbroek <nstapelbroek@gmail.com>
2019-07-21 19:10:22 +0200

    Use LaravelSessionStore in the SessionStateHandler.
    Also, by binding them both to your container you can extend or overwrite the behaviour a bit more easily. Which should help with testing or implemting different auth flows.

--- src/Auth0/Login/LoginServiceProvider.php
+++ src/Auth0/Login/LoginServiceProvider.php
@@ -2,11 +2,14 @@
 
 namespace Auth0\Login;
 
-use Illuminate\Support\ServiceProvider;
 use Auth0\SDK\API\Helpers\ApiClient;
 use Auth0\SDK\API\Helpers\InformationHeaders;
+use Auth0\SDK\API\Helpers\State\SessionStateHandler;
+use Auth0\SDK\Store\StoreInterface;
+use Illuminate\Support\ServiceProvider;
 
-class LoginServiceProvider extends ServiceProvider {
+class LoginServiceProvider extends ServiceProvider
+{
 
     const SDK_VERSION = "4.0.4";
 
@@ -49,12 +52,24 @@ class LoginServiceProvider extends ServiceProvider {
      */
     public function register()
     {
+        $this->app->bind(StoreInterface::class, function () {
+            return new LaravelSessionStore();
+        });
+
+        $this->app->bind(SessionStateHandler::class, function ($app) {
+            return new SessionStateHandler($app->make(LaravelSessionStore::class));
+        });
+
         // Bind the auth0 name to a singleton instance of the Auth0 Service
-        $this->app->singleton(Auth0Service::class, function () {
-              return new Auth0Service();
+        $this->app->singleton(Auth0Service::class, function ($app) {
+            return new Auth0Service(
+                $app->make('config')->get('laravel-auth0'),
+                $app->make(StoreInterface::class),
+                $app->make(SessionStateHandler::class)
+            );
         });
         $this->app->singleton('auth0', function () {
-              return $this->app->make(Auth0Service::class);
+            return $this->app->make(Auth0Service::class);
         });
 
         // When Laravel logs out, logout the auth0 SDK trough the service
