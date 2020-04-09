65c78bd08b686da2fdeb93f0ad0162517347cdc9
Josh Cunningham <josh@joshcanhelp.com>
2020-04-09 09:48:54 -0700

    SDK version and CHANGELOG

--- src/Auth0/Login/LoginServiceProvider.php
+++ src/Auth0/Login/LoginServiceProvider.php
@@ -14,7 +14,7 @@ use Illuminate\Support\ServiceProvider;
 class LoginServiceProvider extends ServiceProvider
 {
 
-    const SDK_VERSION = "5.4.0";
+    const SDK_VERSION = "6.0.0";
 
     /**
      * Bootstrap the application events.
