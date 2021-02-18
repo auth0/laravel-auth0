733f4ac45404ced93f8855b726c971798a7425dd
Evan Sims <evan.sims@auth0.com>
2021-02-18 15:17:18 -0500

    Update LoginServiceProvider.php

--- src/Auth0/Login/LoginServiceProvider.php
+++ src/Auth0/Login/LoginServiceProvider.php
@@ -14,7 +14,7 @@ use Illuminate\Support\ServiceProvider;
 class LoginServiceProvider extends ServiceProvider
 {
 
-    const SDK_VERSION = '6.2.0';
+    const SDK_VERSION = '6.3.0';
 
     /**
      * Bootstrap the application events.
