d662f777de116df5479425971a0a7088eebc404b
Evan Sims <hello@evansims.com>
2022-01-10 11:24:43 -0600

    Setup test suite environment

---
+++ tests/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Laravel polyfills, since we don't want to test against the complete framework suite,
+ * just fill in the method gaps with placeholders.
+ */
+
+if (! function_exists('auth')) {
+    function auth(... $params) {
+
+    }
+}
+
+if (! function_exists('abort')) {
+    function abort(... $params) {
+
+    }
+}
