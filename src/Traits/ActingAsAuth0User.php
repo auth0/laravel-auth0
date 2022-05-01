19ba72c57c7562894d1833c475e1a17531de4726
le-bru <99329443+le-bru@users.noreply.github.com>
2022-05-01 22:13:31 +0200

    added ActingAsAuth0User Trait that can be used in HTTP tests (#276)
    * added ActingAsAuth0User Trait that can be used in HTTP tests for authorizing requests as auth0 user with custom attributes set
    
    * fixed code styling issues
    
    * Fix linter warnings on code style
    
    Co-authored-by: Leonhard Brunner <leonhard.brunner@oeservice.eu>
    Co-authored-by: Evan Sims <hello@evansims.com>

---
+++ src/Traits/ActingAsAuth0User.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Auth0\Laravel\Traits;
+
+use Auth0\Laravel\Model\Stateless\User;
+use Auth0\Laravel\StateInstance;
+use Illuminate\Contracts\Auth\Authenticatable as UserContract;
+
+trait ActingAsAuth0User
+{
+    abstract public function actingAs(
+        UserContract $user,
+        $guard = null
+    );
+
+    /**
+     * use this method to impersonate a specific auth0 user
+     * if you pass an attributes array, it will be merged with a set of default values
+     *
+     * @param array $attributes
+     *
+     * @return mixed
+     */
+    public function actingAsAuth0User(
+        array $attributes = []
+    ) {
+        $defaults = [
+            'sub' => 'some-auth0-user-id',
+            'azp' => 'some-auth0-appplication-client-id',
+            'iat' => time(),
+            'exp' => time() + 60 * 60,
+            'scope' => '',
+        ];
+
+        $auth0user = new User(array_merge($defaults, $attributes));
+
+        if ($auth0user->getAttribute('scope')) {
+            app()->make(StateInstance::class)->setAccessTokenScope(explode(' ', $auth0user->getAttribute('scope')));
+        }
+
+        return $this->actingAs($auth0user, 'auth0');
+    }
+}
