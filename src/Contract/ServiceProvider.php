7a46ba93cbe0d37b09f1943d29c1607f63dae1a3
Evan Sims <hello@evansims.com>
2023-03-27 15:52:01 -0500

    Update ServiceProvider.php

--- src/Contract/ServiceProvider.php
+++ src/Contract/ServiceProvider.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Auth0\Laravel\Contract;
 
-use Illuminate\Contracts\Support\DeferrableProvider;
-
-interface ServiceProvider extends DeferrableProvider
+interface ServiceProvider
 {
 }
