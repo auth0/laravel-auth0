de2ebbeb6ba4c92934686290925598158d14ecc7
Lucas de Araújo Silva <l.de-araujosilva@cyberenergia.com>
2024-11-25 06:40:13 -0300

    docs(facade): added methods documentation

--- src/Facade/Auth0.php
+++ src/Facade/Auth0.php
@@ -9,6 +9,21 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Facade for the Auth0 SDK.
  *
+ * @method static \Auth0\SDK\Configuration\SdkConfiguration getConfiguration()
+ * @method static null|object getCredentials()
+ * @method static null|string getGuardConfigurationKey()
+ * @method static \Auth0\SDK\Contract\Auth0Interface getSdk()
+ * @method static \Auth0\SDK\Contract\API\ManagementInterface management()
+ * @method static self setGuardConfigurationKey(null|string $guardConfigurationKey = null)
+ * @method static \Auth0\SDK\Contract\Auth0Interface setSdk(\Auth0\SDK\Contract\Auth0Interface $sdk)
+ * @method static self reset()
+ * @method static self setConfiguration(\Auth0\SDK\Configuration\SdkConfiguration|array|null $configuration = null)
+ *
+ * @see \Auth0\Laravel\Service
+ * @see \Auth0\Laravel\ServiceAbstract
+ * @see \Auth0\Laravel\Entities\InstanceEntityAbstract
+ * @see \Auth0\Laravel\Entities\InstanceEntityTrait
+ *
  * @codeCoverageIgnore
  *
  * @api
@@ -18,7 +33,7 @@ final class Auth0 extends Facade
     /**
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'auth0';
     }
