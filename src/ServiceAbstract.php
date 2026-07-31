01bee77bdd93b0836e4fa4f01563ad3fe07f0cea
Snehil Kishore <snehil.kishore@okta.com>
2026-07-31 18:35:44 +0530

    Release 8.0.0-beta.0 (#498)

--- src/ServiceAbstract.php
+++ src/ServiceAbstract.php
@@ -22,7 +22,7 @@ abstract class ServiceAbstract extends InstanceEntityAbstract
      *
      * @var string
      */
-    public const VERSION = '7.22.0';
+    public const VERSION = '8.0.0-beta.0';
 
     /**
      * Decode a PSR-7 HTTP Response Message containing a JSON content body to a PHP array. Returns null if the response was not successful, or the response body was not JSON.
